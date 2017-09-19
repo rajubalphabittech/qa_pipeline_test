518ff70f96b3aa7b85fb436744833feb40d48b3c
zack <zack@matterport.com>
2017-09-19 15:14:23 -0700

    shell script test runner test

---
+++ tests/proTests.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+
+# source the unit test for scripts functions
+. jshutest.inc
+
+
+# helper function
+# remove host and date lines from buildnumber.txt file
+stripHostDate() {
+	source="$1"
+	dest="$2"
+	grep -v '^BuildHost:' $source | grep -v '^Date' >$dest
+}
+
+# In this one, we don't source the other script because
+# we're not going to test pieces of it. We're just going
+# to run the script and evaluate what it does.
+##############################################################
+# unit test functions
+proEmptymesh_Test() {
+	# process the model
+	ls -l > text.log
+    a=10
+	b=10
+	if [ $a == $b ]; then
+		return ${jshuFAIL}
+	fi
+		echo "PASS"
+		return ${jshuPASS}
+}
+
+pro2Emptymesh_Test() {
+	# process the model
+	{ # try
+		manifest_edit
+
+	} || { # catch
+		return ${jshuFAIL}
+	}
+	return ${jshuPASS}	
+}
+
+
+##############################################################
+# main
+##############################################################
+# initialize testsuite
+jshuInit
+
+# run unit tests on script
+jshuRunTests
+
+# result summary
+jshuFinalize
+
+echo Done.
+echo
+let tot=failed+errors
+exit $tot
