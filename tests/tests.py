bc1795e4f8033d056210c6b0ac12763fa29729f9
zack <zack@matterport.com>
2017-09-20 15:12:00 -0700

    sigh

--- tests/tests.py
+++ tests/tests.py
@@ -10,12 +10,18 @@ except ImportError:
     import unittest
 
 
+
+
+
 # call the testcases
 class ProTests(unittest.TestCase):
 
     def test_Pro10sweeps(self):
-      with self.assertRaises(SomeException):
+      try:
         call(["cd", "/mnt/dev/mp_vision-build/deploy/", "&&", "./vision_mesh_code.work", "/mnt/dev/mp_vision-build/deploy/", "/mnt/dev/testResults/emptymesh/"])
+        self.failUnless(True)
+      except:
+        self.failUnless(False)
 
 
 
