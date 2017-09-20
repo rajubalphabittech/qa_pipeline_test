734a13c84a523ffdb78372136941fabbab096665
zack <zack@matterport.com>
2017-09-20 16:35:50 -0700

    more

--- tests/tests.py
+++ tests/tests.py
@@ -21,6 +21,10 @@ class ProTests(unittest.TestCase):
         print ("pass...start")
         cmd = "cd /mnt/dev/mp_vision-build/deploy/ && ./vision_mesh_code.work /mnt/dev/mp_vision-build/deploy/ /mnt/dev/testResults/emptymesh/"
         process = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        process.wait()
+        out, err = process.communicate()
+        print ("Return code: ", process.returncode)
+        print (out.rstrip(), err.rstrip())
         print ("pass...end")
       except:
         print ("Fail")
