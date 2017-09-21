61beffc31a7bb81b23e39e7df6d506767d7e28c1
zack <zack@matterport.com>
2017-09-21 12:32:06 -0700

    why you no fail

--- tests/proTest2.sh
+++ tests/proTest2.sh
@@ -8,7 +8,7 @@
 proEmptymesh() {
    # process the model
    { # try
-       cd /mnt/dev/mp_vision-build/deploy/ && ./vision_mesh_code.work /mnt/dev/mp_vision-build/deploy/ /mnt/dev/testResults/emptymesh/
+       cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' .1 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
    } || { # catch
        return 1 
    }
