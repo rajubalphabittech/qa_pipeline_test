f897e12fb2d4ba2aec58d357274edf1bbc749d5c
zack <zack@matterport.com>
2017-09-21 12:36:25 -0700

    bash -xe

--- tests/proTest2.sh
+++ tests/proTest2.sh
@@ -8,7 +8,7 @@
 proEmptymesh() {
    # process the model
    { # try
-       cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' .1 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
+       bash -xe cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' .1 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
    } || { # catch
        return 1 
    }
