187104fd047575b9a7fc3eb3f0b906df70a26ebe
zack <zack@matterport.com>
2017-09-20 22:02:52 -0700

    test

--- tests/tests.py
+++ tests/tests.py
@@ -18,11 +18,13 @@ class ProTests(unittest.TestCase):
 
     def test_Pro10sweeps(self):
       try:
+        print ("process image")
         cmd = "cd /mnt/dev/mp_vision-build/deploy/ && ./vision_mesh_code.work /mnt/dev/mp_vision-build/deploy/ /mnt/dev/testResults/emptymesh/"
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         process.wait()
         output = process.stderr.read()
         print (output)
+        print ("test image")
         cmd = "cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' .1 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'"
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         process.wait()
