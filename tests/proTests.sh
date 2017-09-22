ed1687446990c8e08dc1b22690bc3a3e82fbe0ac
zack <zack@matterport.com>
2017-09-22 11:14:43 -0700

    one pass one fail

--- tests/proTests.sh
+++ tests/proTests.sh
@@ -5,3 +5,10 @@
 	cd /mnt/dev/qa/automation/jenkins/ && run ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
 	[ $status = 0 ]
 }
+
+
+@test "Pro2 empty mesh" {
+# test image
+	cd /mnt/dev/qa/automation/jenkins/ && run ./imageCompare.sh '/mnt/dev' 2 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
+	[ $status = 0 ]
+}
