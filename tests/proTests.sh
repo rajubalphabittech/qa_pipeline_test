31226afa5abc76f760c2dc08db3456ad0f0de0f3
zack <zack@matterport.com>
2017-09-22 15:57:22 -0700

    revert

--- tests/proTests.sh
+++ tests/proTests.sh
@@ -18,14 +18,7 @@ proEemptyMesh() {
 	return ${jshuPASS}	
 }
 
-proEemptyMesh_image() {
-	{ # try
-		cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
-	} || { # catch
-		return ${jshuFAIL}
-	}
-	return ${jshuPASS}	
-}
+
 
 
 ##############################################################
