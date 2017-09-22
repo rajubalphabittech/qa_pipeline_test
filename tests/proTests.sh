28c8cb92625e1d1281b7751ccb026fb9db7fa79d
zack <zack@matterport.com>
2017-09-22 15:51:51 -0700

    separate tests

--- tests/proTests.sh
+++ tests/proTests.sh
@@ -7,19 +7,24 @@
 # unit test functions
 
 
-Emptymesh_FAIL_Test() {
+proEemptyMesh() {
 	{ # try
 		# process the model
-		cd /mnt/dev/mp_vision-build/deploy/ && ./vision_mesh_code.work /mnt/dev/mp_vision-build/deploy/ /mnt/dev/testResults/emptymesh/
-		# test images
-		cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
+		cd /mnt/dev/mp_vision-build/deploy/ && ./vision_mesh_code.work /mnt/dev/mp_vision-build/deploy/ /mnt/dev/testResults/emptymesh/	
 	} || { # catch
 		return ${jshuFAIL}
 	}
 	return ${jshuPASS}	
 }
 
-
+proEemptyMesh_image() {
+	{ # try
+		cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
+	} || { # catch
+		return ${jshuFAIL}
+	}
+	return ${jshuPASS}	
+}
 
 
 ##############################################################
