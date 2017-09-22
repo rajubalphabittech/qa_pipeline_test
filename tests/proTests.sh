562a68cfc936fff89a747ae1d33c6a85eda6287d
zack <zack@matterport.com>
2017-09-22 11:32:07 -0700

    testing

--- tests/proTests.sh
+++ tests/proTests.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bats
 
-@test "Pro empty mesh" {
-# test image
-	cd /mnt/dev/qa/automation/jenkins/ && run ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
-	[ $status = 0 ]
+
+@test "no arguments prints usage instructions" {
+  run bats
+  [ $status -eq 1 ]
+  [ $(expr "${lines[1]}" : "Usage:") -ne 0 ]
 }
 
+@test "-v and --version print version number" {
+  run bats -v
+  [ $status -eq 0 ]
+  [ $(expr "$output" : "Bats [0-9][0-9.]*") -ne 0 ]
+}
 
-@test "Pro2 empty mesh" {
-# test image
-	cd /mnt/dev/qa/automation/jenkins/ && run ./imageCompare.sh '/mnt/dev' 2 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
-	[ $status = 0 ]
+@test "-h and --help print help" {
+  run bats -h
+  [ $status -eq 0 ]
+  [ "${#lines[@]}" -gt 3 ]
 }
