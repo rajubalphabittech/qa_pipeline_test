ec5ac73076b66bc45cde0af2a1d67d0be4db7c81
zack <zack@matterport.com>
2017-09-22 10:49:21 -0700

    format

--- tests/proTests.sh
+++ tests/proTests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bats
 
 @test "Pro empty mesh" {
-        # test image
-        cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
-        [ $status -eq 0 ]
+# test image
+	run cd /mnt/dev/qa/automation/jenkins/ && ./imageCompare.sh '/mnt/dev' 4 'pan/high' '72e2e8bdf87c45e29d023e7e18af1cc1_skybox1.jpg' '01sweep'
+	[ $status = 0 ]
 }
